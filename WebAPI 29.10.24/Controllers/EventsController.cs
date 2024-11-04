aa81f9beb18f391adc1c59cb73f01fdcbebd014c
mimi <m07213984@gmail.com>
2024-11-04 22:22:15 +0200

    28/10

---
+++ WebAPI 29.10.24/Controllers/EventsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;    
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI_29._10._24.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        public static List<Event> eventList = new List<Event>();
+//===================================================================
+        [HttpGet]
+        public List<Event> Get()
+        {
+            return eventList;
+        }
+//===================================================================
+        [HttpPost]
+        public void Post([FromBody] Event e)
+        {
+            int i = eventList.Count;
+            e.Id = i + 1;
+            eventList.Add(e);   
+        }
+//===================================================================
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Event e)
+        {
+            var existingEvent= eventList.Find(ev=>ev.Id == id);
+            if (existingEvent != null)
+            {
+              existingEvent.Title = e.Title; // דוגמה, עדכן את השדה Name
+              existingEvent.Start = e.Start;
+              existingEvent.End = e.End;
+            }
+            return;
+
+        }
+//===================================================================
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            eventList.Remove(eventList.Find(ev => ev.Id == id));
+        }
+    }
+}
---
+++ WebAPI 29.10.24/Event.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPI_29._10._24
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+}
